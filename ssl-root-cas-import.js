df09d8788c85fae41d0004ba8dc9ec739dd5d4f8
Antoine pous <Antoine-Pous@users.noreply.github.com>
2015-03-16 17:13:27 +0100

    Update ssl-root-cas-import.js

--- ssl-root-cas-import.js
+++ ssl-root-cas-import.js
@@ -1,1 +1,24 @@
+/*
+ * This file is part of Node Free Mobile SMS API.
+
+ * Node Free Mobile SMS API is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * Node Free Mobile SMS API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Node Free Mobile SMS API. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var GNU_LGPL_V3 = "Node Free Mobile SMS API Copyright (C) 2015  Antoine `Gecko` Pous" + 
+                  "This program comes with ABSOLUTELY NO WARRANTY." +
+                  "This is free software, and you are welcome to redistribute it" +
+                  "under GNU Lesser GENERAL PUBLIC LICENSE Version 3, " +
+                  "see https://github.com/Antoine-Pous/node-free-mobile-sms-api/blob/master/LICENSE-LGPL-v3.md for details.";
+console.log(GNU_LGPL_V3);
 var ssl = require("ssl-root-cas/latest").inject();
